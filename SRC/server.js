122e19af801d65beca8fbdb295054f29e1ff2a73
Donadoni10 <matheusdonadoni5@gmail.com>
2024-05-22 11:37:57 -0300

    first

---
+++ SRC/server.js
@@ -0,0 +1,13 @@
+
+import express from 'express';
+import mainRoutes from './routes/main.js';
+import dotenv from 'dotenv';
+ 
+dotenv.config();
+ 
+const server = express();
+server.use(express.json());
+ 
+server.use(mainRoutes);
+ 
+server.listen(process.env.PORT);
